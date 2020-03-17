27db2b57147c6e40ef41c0ed6d742e3abe0a0e4e
Angry Segin <lukasz-segin@o2.pl>
2020-03-17 20:05:46 +0100

    Tworzenie obiektu Vue

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-let counter = 1;
-let container = document.createElement("div");
-container.classList.add("text-center", "p-3");
-let msg = document.createElement("h1");
-msg.classList.add("bg-primary", "text-white", "p-3");
-msg.textContent = "Przycisk nie został wciśnięty";
-let button = document.createElement("button");
-button.textContent = "Wciśnij mnie";
-button.classList.add("btn", "btn-secondary");
-button.onclick = () => msg.textContent = `Liczba wciśnięć: ${counter++}`;
-container.appendChild(msg);
-container.appendChild(button);
-let app = document.getElementById("app");
-app.parentElement.replaceChild(container, app);
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
+import Vue from "vue";
+new Vue({
+  el: "#app",
+  template: `<div class="text-center p-3">
+<h1 class="bg-secondary text-white p-3">
+Vue: Przycisk nie zotał wciśnięty
+</h1>
+<button class="btn btn-secondary">
+Wciśnij mnie
+</button>
+</div>`
+});
