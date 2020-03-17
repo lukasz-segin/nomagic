60d58d9e14b35cadd88cf2b86bda3374852bff96
Angry Segin <lukasz-segin@o2.pl>
2020-03-17 20:17:39 +0100

    Stosowanie obiektu Vue. Dodawanie funkcji obsługi zdarzenia. Modyfikacja komunikatu.

--- src/main.js
+++ src/main.js
@@ -4,10 +4,23 @@ new Vue({
   el: "#app",
   template: `<div class="text-center p-3">
 <h1 class="bg-secondary text-white p-3">
-Vue: Przycisk nie zotał wciśnięty
+{{ message }}
 </h1>
-<button class="btn btn-secondary">
+<button class="btn btn-secondary" v-on:click="handleClick">
 Wciśnij mnie
 </button>
-</div>`
+</div>`,
+  data: {
+    counter: 0
+  },
+  methods: {
+    handleClick() {
+      this.counter++;
+    }
+  },
+  computed: {
+    message() {
+      return this.counter === 0 ? "Przycisk nie został wciśnięty" : `Liczba wciśnięć: ${this.counter}`;
+    }
+  }
 });
