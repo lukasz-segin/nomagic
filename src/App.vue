c644d4e8af539ac486919ddcd04b0f52c6c84345
Angry Segin <lukasz-segin@o2.pl>
2020-03-18 20:32:05 +0100

    Oddzielanie szablonu od kodu JavaScript. Tworzenie odrębnych plików JavaScript i HTML.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
+<template>
+  <div class="text-center p-3">
+    <h1 class="btn-secondary text-white p-3">
+      {{ message }}
+    </h1>
+    <button class="btn btn-secondary" v-on:click="handleClick">
+      Wciśnij mnie
+    </button>
+  </div>
+</template>
+
 <script>
   export default {
-    template: `
-      <div class="text-center p-3">
-        <h1 class="btn-secondary text-white p-3">
-          {{ message }}
-        </h1>
-        <button class="btn btn-secondary" v-on:click="handleClick">
-          Wciśnij mnie
-        </button>
-      </div>`,
     data: function () {
       return {
         counter: 0
